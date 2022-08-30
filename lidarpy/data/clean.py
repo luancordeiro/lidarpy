b32c83585c8d0b3b31357888ecbd10057efd5778
luan <luanpcordeiro0@gmail.com>
2022-08-30 15:38:00 -0300

    add função agrupar nbins

--- lidarpy/data/clean.py
+++ lidarpy/data/clean.py
@@ -21,3 +21,25 @@ def remove_background(ds, alt_ref: list):
                   .mean("altitude"))
 
     return ds - background
+
+
+def groupby_nbins(ds, n_bins):
+    """Agrupa o ds a cada n_bins
+
+    O código está meio feio, mas aparentemente funciona
+    """
+    if n_bins in [0, 1]:
+        return ds
+
+    alt = ds.coords["altitude"].data
+
+    mean = alt[:n_bins].mean()
+
+    print(mean)
+
+    return (ds
+            .assign_coords(altitude=np.arange(len(alt)) // n_bins)
+            .groupby("altitude")
+            .mean()
+            .assign_coords(altitude=lambda x: [alt[i * n_bins:(i + 1) * n_bins].mean() for i in range(len(x.altitude))])
+          )
