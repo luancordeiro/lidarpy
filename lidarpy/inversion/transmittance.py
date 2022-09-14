6f9fe31a23e6f856b861176c2c8ef6371ef759ef
luan <luanpcordeiro0@gmail.com>
2022-09-14 14:58:04 -0300

    changes in the computation of the rcs below and above the cloud

--- lidarpy/inversion/transmittance.py
+++ lidarpy/inversion/transmittance.py
@@ -2,6 +2,7 @@ import xarray as xr
 import numpy as np
 from scipy.integrate import trapz
 from lidarpy.data.alpha_beta_mol import AlphaBetaMolecular
+import matplotlib.pyplot as plt
 
 
 class Transmittance:
@@ -24,8 +25,18 @@ class Transmittance:
 
         self.z = lidar_data.coords["altitude"].data
 
+        z_lims += [z_lims[0] - 500, z_lims[1] + 500]
+        print(z_lims)
+
         z_ref = lidar_data.coords["altitude"].sel(altitude=z_lims, method="nearest").data
-        self._ref = np.where((self.z == z_ref[0]) | (self.z == z_ref[1]))[0]
+
+        self._ref_base = np.where((self.z == z_ref[0]) | (self.z == z_ref[2]))[0]
+        self._ref_top = np.where((self.z == z_ref[1]) | (self.z == z_ref[3]))[0]
+
+        print(self._ref_base)
+        print(self._ref_top)
+        print(self.z[self._ref_base])
+        print(self.z[self._ref_top])
 
         self._get_molecular_alpha_beta(p_air, t_air, wavelength * 1e-9, co2ppmv)
 
@@ -36,17 +47,26 @@ class Transmittance:
         alpha_beta_mol = AlphaBetaMolecular(p_air, t_air, wavelength, co2ppmv)
         self._alpha, self._beta, _ = alpha_beta_mol.get_params()
 
-    def fit(self, delta_z=10):
-        """Para calcular o tau, estou pegando o sinal médio dos 10 bins abaixo da camada
-        e os 10 bins diretamente acima da camada"""
+    def _ref_value(self, y, ref):
+        p = np.poly1d(np.polyfit(self.z[ref[0]: ref[1]], y[ref[0]: ref[1]], 1))
+
+        plt.plot(self.z[ref[0]: ref[1]], y[ref[0]: ref[1]], ".")
+        plt.plot(self.z[ref[0]: ref[1]], p(self.z[ref[0]: ref[1]]), "-")
+        plt.plot(self.z[(ref[0] + ref[1]) // 2], p(self.z[(ref[0] + ref[1]) // 2]), "*")
+        plt.show()
+
+        return p(self.z[(ref[0] + ref[1]) // 2])
+
+    def fit(self):
         s = self._range_corrected_signal()
-        s0, s1 = s[self._ref[0] - delta_z:self._ref[0]].mean(), s[self._ref[1]:self._ref[1] + delta_z].mean()
-        beta0, beta1 = (self._beta[self._ref[0] - 10:self._ref[0]].mean(),
-                        self._beta[self._ref[1]:self._ref[1] + 10].mean())
+        s0, s1 = self._ref_value(s, self._ref_base), self._ref_value(s, self._ref_top)
+        beta0, beta1 = self._ref_value(self._beta, self._ref_base), self._ref_value(self._beta, self._ref_top)
 
         transmittance_factor = (s1 * beta0 / (s0 * beta1)
-                                * np.exp(2 * trapz(x=self.z[self._ref[0]:self._ref[1] + 1],
-                                                   y=self._alpha[self._ref[0]:self._ref[1] + 1])))
+                                * np.exp(2 * trapz(x=self.z[(self._ref_base[0] + self._ref_base[1]) // 2:
+                                                            (self._ref_top[0] + self._ref_top[1]) // 2],
+                                                   y=self._alpha[(self._ref_base[0] + self._ref_base[1]) // 2:
+                                                                 (self._ref_top[0] + self._ref_top[1]) // 2])))
 
         self.tau = -0.5 * np.log(transmittance_factor)
 
