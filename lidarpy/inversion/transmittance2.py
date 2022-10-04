c85cd06f6440984310706fab5c4812374698db19
luan <luancordeiro0@gmail.com>
2022-10-04 13:38:39 -0300

    changes in the parameters

---
+++ lidarpy/inversion/transmittance2.py
@@ -0,0 +1,68 @@
+import xarray as xr
+import numpy as np
+from scipy.integrate import trapz
+from lidarpy.data.manipulation import molecular_model
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+class Transmittance:
+    _alpha = None
+    _beta = None
+    tau = None
+
+    def __init__(self,
+                 lidar_data: xr.DataArray,
+                 cloud_lims: list,
+                 wavelength: int,
+                 p_air: np.ndarray,
+                 t_air: np.ndarray,
+                 pc=True,
+                 co2ppmv: int = 392,
+                 delta_z=200):
+        if "wavelength" in lidar_data.dims:
+            self.signal = lidar_data.sel(wavelength=f"{wavelength}_{int(pc)}").data
+        else:
+            self.signal = lidar_data.data
+
+        self.z = lidar_data.coords["altitude"].data
+
+        fit_ref = [cloud_lims[0] - 2000, cloud_lims[0]]
+
+        molecular_signal = molecular_model(lidar_data,
+                                                wavelength,
+                                                p_air,
+                                                t_air,
+                                                fit_ref,
+                                                co2ppmv)
+
+        molecular_rcs = molecular_signal * self.z ** 2
+
+        transmittance_ref = cloud_lims[1] + delta_z
+        transmittance_ref = lidar_data.coords["altitude"].sel(altitude=transmittance_ref, method="nearest").data
+        transmittance_ref = np.where(self.z == transmittance_ref)[0][0]
+
+        rcs = self.signal * self.z ** 2
+        plt.plot(self.z, rcs, "b-", label="Lidar profile")
+        plt.plot(self.z[fit_ref[0]:fit_ref[1]], rcs[fit_ref[0]:fit_ref[1]], "y--", label="Fit region")
+        # plt.plot(self.z[cloud_lims], rcs[cloud_lims], "b*", label="Cloud lims")
+        plt.plot(self.z, molecular_rcs, "k-", label="Mol. profile")
+        plt.plot(self.z[transmittance_ref: transmittance_ref + 150], rcs[transmittance_ref: transmittance_ref + 150],
+                 "y*", label="transmittance")
+        plt.grid()
+        plt.legend()
+        plt.show()
+
+        transmittance_z = self.z[transmittance_ref: transmittance_ref + 150]
+        transmittance = rcs[transmittance_ref: transmittance_ref + 150] / molecular_rcs[transmittance_ref: transmittance_ref + 150]
+
+        mean = transmittance.mean()
+        std = transmittance.std(ddof=1)
+
+        plt.plot(transmittance_z, transmittance)
+        plt.plot([transmittance_z[0], transmittance_z[-1]], [mean, mean], "k--")
+        plt.title(f"mean value = {mean}")
+        plt.show()
+
+        print("AOD =", -0.5 * np.log(mean))
+
---
+++ lidarpy/tests/test_transmittance_2.py
@@ -0,0 +1,42 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import cumtrapz
+from lidarpy.inversion.transmittance2 import Transmittance
+from lidarpy.data.manipulation import remove_background
+from lidarpy.inversion.klett import Klett
+from lidarpy.plot.plotter import plot_3graph_std, compare_w_sol
+
+weak_cloud = 1
+
+link = ["http://lalinet.org/uploads/Analysis/Concepcion2014/SynthProf_cld6km_abl1500.txt",
+        "http://lalinet.org/uploads/Analysis/Concepcion2014/SynthProf_cld6km_abl1500_v2.txt"]
+
+if weak_cloud:
+    df_sol = pd.read_csv("data/sol_lalinet_weak_cloud.txt", delimiter="\t")
+
+my_data = np.genfromtxt(link[weak_cloud])
+
+ds = xr.DataArray(my_data[:, 1], dims=["altitude"])
+ds.coords["altitude"] = my_data[:, 0]
+print(ds.shape)
+
+df_sonde = pd.read_csv("data/sonde_lalinet.txt", delimiter="\t")
+
+df_sonde = (df_sonde
+            .assign(pressure=lambda x: x.pressure * 100)
+            .assign(temperature=lambda x: x.temperature + 273.15)
+            [["altitude", "pressure", "temperature"]])
+
+print()
+print(df_sonde.head())
+print()
+
+ds = ds.pipe(remove_background, [10_000, 14_000])
+
+tau = Transmittance(ds,
+                    [5800, 6150],
+                    355,
+                    df_sonde["pressure"].to_numpy(),
+                    df_sonde["temperature"].to_numpy())
